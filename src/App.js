2d391bb040f25de1c070ac0a2f50f9e2642ec801
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-04-24 16:34:14 +0530

    header component add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-
+import Header from "./Header"
 function App() {
   return (
     //BEM class convention
     <div className="app">
       <h1>LEts build</h1>
+      <Header />
       {/** HEADER
        * Tinder card
        * swipebutton
