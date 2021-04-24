3f59fea3692e3d1884ce507674ade9a2fad71b67
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-04-24 16:17:52 +0530

    react setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    //BEM class convention
+    <div className="app">
+      <h1>LEts build</h1>
+      {/** HEADER
+       * Tinder card
+       * swipebutton
+       */}
+    </div>
+  );
+}
+
+export default App;
